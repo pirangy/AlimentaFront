7e39eee9bede076124ee2ebc2ba472f670968e01
Ana Paula Souza <anapaula_sdias@hotmail.com>
2021-10-04 09:25:37 -0300

    Model

---
+++ src/app/model/User.ts
@@ -0,0 +1,12 @@
+export class User{
+  
+  public  id: number
+  public  nome: string
+  public cnpj: string
+  public usuario: string
+  public email: string
+  public senha:string
+  public foto:string
+  public tipo:string
+
+}
