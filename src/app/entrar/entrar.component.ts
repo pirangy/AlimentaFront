8db17541a6f7bc499e38150b32c7e84f46c53c14
HenriqueUriel <riikilopes262@hotmail.com>
2021-10-04 23:01:02 -0300

    anotação campo CNPJ no cadastrar, formato de cadastro 88.051.906/0001-86

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -24,17 +24,13 @@ export class EntrarComponent implements OnInit {
   }
 
   entrar(){
-    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin)=>{
+    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin) =>{
       this.userLogin = resp
-
 
       environment.token = this.userLogin.token
       environment.nome = this.userLogin.nome
       environment.foto =  this.userLogin.foto
       environment.id = this.userLogin.id
-
-
-
 
       this.router.navigate(['/inicio'])
     }, erro =>{
